13bcb2c7ead91c21bd071eb92769831989a5a9d6
wujingpeng <wujingpeng@youzan.com>
2018-09-11 17:16:40 +0800

    feat:
    自定义 commit message

--- buildCommit.js
+++ buildCommit.js
@@ -22,6 +22,7 @@ module.exports = function buildCommit(answers, config) {
   }
 
   function addSubject(subject) {
+     if (!subject) return '';
     return subject.trim();
   }
 
--- questions.js
+++ questions.js
@@ -18,11 +18,11 @@ module.exports = {
     var messages = config.messages || {};
 
     messages.type = messages.type || 'Select the type of change that you\'re committing:';
-    messages.scope = messages.scope || '\nDenote the SCOPE of this change (optional):';
-    messages.customScope = messages.customScope || 'Denote the SCOPE of this change:';
-    messages.subject = messages.subject || 'Write a SHORT, IMPERATIVE tense description of the change:\n';
+    // messages.scope = messages.scope || '\nDenote the SCOPE of this change (optional):';
+    // messages.customScope = messages.customScope || 'Denote the SCOPE of this change:';
+    // messages.subject = messages.subject || 'Write a SHORT, IMPERATIVE tense description of the change:\n';
     messages.body = messages.body || 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n';
-    messages.breaking = messages.breaking || 'List any BREAKING CHANGES (optional):\n';
+    // messages.breaking = messages.breaking || 'List any BREAKING CHANGES (optional):\n';
     messages.footer = messages.footer || 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n';
     messages.confirmCommit = messages.confirmCommit || 'Are you sure you want to proceed with the commit above?';
 
@@ -33,80 +33,76 @@ module.exports = {
         message: messages.type,
         choices: config.types
       },
-      {
-        type: 'list',
-        name: 'scope',
-        message: messages.scope,
-        choices: function(answers) {
-          var scopes = [];
-          if (scopeOverrides[answers.type]) {
-            scopes = scopes.concat(scopeOverrides[answers.type]);
-          } else {
-            scopes = scopes.concat(config.scopes);
-          }
-          if (config.allowCustomScopes || scopes.length === 0) {
-            scopes = scopes.concat([
-              new cz.Separator(),
-              { name: 'empty', value: false },
-              { name: 'custom', value: 'custom' }
-            ]);
-          }
-          return scopes;
-        },
-        when: function(answers) {
-          var hasScope = false;
-          if (scopeOverrides[answers.type]) {
-            hasScope = !!(scopeOverrides[answers.type].length > 0);
-          } else {
-            hasScope = !!(config.scopes && (config.scopes.length > 0));
-          }
-          if (!hasScope) {
-            answers.scope = 'custom';
-            return false;
-          } else {
-            return isNotWip(answers);
-          }
-        }
-      },
-      {
-        type: 'input',
-        name: 'scope',
-        message: messages.customScope,
-        when: function(answers) {
-          return answers.scope === 'custom';
-        }
-      },
-      {
-        type: 'input',
-        name: 'subject',
-        message: messages.subject,
-        validate: function(value) {
-          var limit = config.subjectLimit || 100;
-          if (value.length > limit) {
-            return 'Exceed limit: ' + limit;
-          }
-          return true;
-        },
-        filter: function(value) {
-          return value.charAt(0).toLowerCase() + value.slice(1);
-        }
-      },
+      // {
+      //   type: 'list',
+      //   name: 'scope',
+      //   message: messages.scope,
+      //   choices: function(answers) {
+      //     var scopes = [];
+      //     if (scopeOverrides[answers.type]) {
+      //       scopes = scopes.concat(scopeOverrides[answers.type]);
+      //     } else {
+      //       scopes = scopes.concat(config.scopes);
+      //     }
+      //     if (config.allowCustomScopes || scopes.length === 0) {
+      //       scopes = scopes.concat([
+      //         new cz.Separator(),
+      //         { name: 'empty', value: false },
+      //         { name: 'custom', value: 'custom' }
+      //       ]);
+      //     }
+      //     return scopes;
+      //   },
+      //   when: function(answers) {
+      //     var hasScope = false;
+      //     if (scopeOverrides[answers.type]) {
+      //       hasScope = !!(scopeOverrides[answers.type].length > 0);
+      //     } else {
+      //       hasScope = !!(config.scopes && (config.scopes.length > 0));
+      //     }
+      //     if (!hasScope) {
+      //       answers.scope = 'custom';
+      //       return false;
+      //     } else {
+      //       return isNotWip(answers);
+      //     }
+      //   }
+      // },
+      // {
+      //   type: 'input',
+      //   name: 'scope',
+      //   message: messages.customScope,
+      //   when: function(answers) {
+      //     return answers.scope === 'custom';
+      //   }
+      // },
+      // {
+      //   type: 'input',
+      //   name: 'subject',
+      //   message: messages.subject,
+      //   validate: function(value) {
+      //     return !!value;
+      //   },
+      //   filter: function(value) {
+      //     return value.charAt(0).toLowerCase() + value.slice(1);
+      //   }
+      // },
       {
         type: 'input',
         name: 'body',
         message: messages.body
       },
-      {
-        type: 'input',
-        name: 'breaking',
-        message: messages.breaking,
-        when: function(answers) {
-          if (config.allowBreakingChanges && config.allowBreakingChanges.indexOf(answers.type.toLowerCase()) >= 0) {
-            return true;
-          }
-          return false; // no breaking changes allowed unless specifed
-        }
-      },
+      // {
+      //   type: 'input',
+      //   name: 'breaking',
+      //   message: messages.breaking,
+      //   when: function(answers) {
+      //     if (config.allowBreakingChanges && config.allowBreakingChanges.indexOf(answers.type.toLowerCase()) >= 0) {
+      //       return true;
+      //     }
+      //     return false; // no breaking changes allowed unless specifed
+      //   }
+      // },
       {
         type: 'input',
         name: 'footer',
